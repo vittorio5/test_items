7515e04d0fe09c9b02a723469b4b09e6ad092e3b
vittorio5 <vittorio.inbox@gmail.com>
2025-02-10 16:06:02 +0700

    stepik - made changes in final task of section3

--- conftest.py
+++ conftest.py
@@ -1,1 +1,29 @@
+import pytest
+from selenium import webdriver
 
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    # записываем значение языка в переменную
+    language = request.config.getoption("language")
+    # переходим по ссылке с указанием нужного языка
+    browser.get(f"https://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
--- test_items.py
+++ test_items.py
@@ -1,1 +1,18 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
+
+def test_add_to_basket_button_exists(browser):
+    # даём время на визуальный осмотр страницы
+    time.sleep(30)
+    # пытаемся найти на странице кнопку добавления в корзину
+    try:
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "button.btn-add-to-basket")))
+        button_exists = True        
+    except:
+        button_exists = False        
+    finally:
+        # Проверяем, была ли найдена кнопка на странице
+        assert button_exists, "Кнопка добавления в корзину отсутствует на странице!"
